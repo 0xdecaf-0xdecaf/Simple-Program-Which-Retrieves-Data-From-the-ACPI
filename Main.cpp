cd8da6c731a9045d809c181d6c841d55d338933f
0xdecaf-0xdecaf <betsvoro@gmail.com>
2024-08-03 12:27:46 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,30 +1,25 @@
 #include <windows.h>
 #include <iostream>
 #include <vector>
-
 void GetAcpiTable(const char* sig) {
-    UINT table_size = GetSystemFirmwareTable('ACPI', *(reinterpret_cast<const UINT*>(sig)), NULL, 0);
-    if (table_size == 0) { // this should never happen
-        return;
-    }
-
-    std::vector<BYTE> table_buffer(table_size);
-
-    UINT result = GetSystemFirmwareTable('ACPI', *(reinterpret_cast<const UINT*>(sig)), table_buffer.data(), table_size);
-    if (result != table_size) { // this shold never happen either
-        std::cerr << "can't retrieve acpi table" << std::endl;
-        return;
-    }
-
-    std::cout << "retrieved acpi table: " << sig << std::endl;
-    for (size_t i = 0; i < table_buffer.size(); ++i) {
-        printf("%02X ", table_buffer[i]);
-        if ((i + 1) % 16 == 0) {
-            printf("\n");
-        }
-    }
+ UINT table_size = GetSystemFirmwareTable('ACPI', *(reinterpret_cast<const UINT*>(sig)), NULL, 0);
+ if (table_size == 0) { // this should never happen
+  return;
+ }
+ std::vector<BYTE> table_buffer(table_size);
+ UINT result = GetSystemFirmwareTable('ACPI', *(reinterpret_cast<const UINT*>(sig)), table_buffer.data(), table_size);
+ if (result != table_size) { // this shold never happen either
+  std::cerr << "can't retrieve acpi table" << std::endl;
+  return;
+ }
+ std::cout << "retrieved acpi table: " << sig << std::endl;
+ for (size_t i = 0; i < table_buffer.size(); ++i) {
+  printf("%02X ", table_buffer[i]);
+  if ((i + 1) % 16 == 0) {
+   printf("\n");
+  }
+ }
 }
-
 int main(int argc, char** argv) {
-    GetAcpiTable("DSDT");
+ GetAcpiTable("DSDT");
 }
