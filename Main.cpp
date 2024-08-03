89fd150831ac94aa85808c26370e5e0106bb7a1e
0xdecaf-0xdecaf <betsvoro@gmail.com>
2024-08-03 12:23:43 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,21 +2,21 @@
 #include <iostream>
 #include <vector>
 
-void GetAcpiTable(const char* signature) {
-    UINT table_size = GetSystemFirmwareTable('ACPI', *(reinterpret_cast<const UINT*>(signature)), NULL, 0);
+void GetAcpiTable(const char* sig) {
+    UINT table_size = GetSystemFirmwareTable('ACPI', *(reinterpret_cast<const UINT*>(sig)), NULL, 0);
     if (table_size == 0) { // this should never happen
         return;
     }
 
     std::vector<BYTE> table_buffer(table_size);
 
-    UINT result = GetSystemFirmwareTable('ACPI', *(reinterpret_cast<const UINT*>(signature)), table_buffer.data(), table_size);
+    UINT result = GetSystemFirmwareTable('ACPI', *(reinterpret_cast<const UINT*>(sig)), table_buffer.data(), table_size);
     if (result != table_size) { // this shold never happen either
         std::cerr << "can't retrieve acpi table" << std::endl;
         return;
     }
 
-    std::cout << "retrieved acpi table: " << signature << std::endl;
+    std::cout << "retrieved acpi table: " << sig << std::endl;
     for (size_t i = 0; i < table_buffer.size(); ++i) {
         printf("%02X ", table_buffer[i]);
         if ((i + 1) % 16 == 0) {
